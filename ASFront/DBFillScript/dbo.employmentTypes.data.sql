bf743328587bfbad53f435e38dbfeae4efbae065
gevorgsanoyan <32333120+gevorgsanoyan@users.noreply.github.com>
2018-03-27 10:51:34 +0400

    Delete dbo.employmentTypes.data.sql

--- ASFront/DBFillScript/dbo.employmentTypes.data.sql
+++
@@ -1,5 +0,0 @@
-SET IDENTITY_INSERT [dbo].[employmentTypes] ON
-INSERT INTO [dbo].[employmentTypes] ([empTypeID], [employment]) VALUES (1, N'ՄԱՍՆԱՎՈՐ ՀԱՏՎԱԾ / ՎԱՐՁՈՒ ԱՇԽԱՏՈՂ')
-INSERT INTO [dbo].[employmentTypes] ([empTypeID], [employment]) VALUES (2, N'ՊԵՏԱԿԱՆ ՀԱՏՎԱԾ')
-INSERT INTO [dbo].[employmentTypes] ([empTypeID], [employment]) VALUES (3, N'ՁԵՌՆԵՐԵՑ / ԻՆՔՆԱԶԲԱՂՎԱԾ')
-SET IDENTITY_INSERT [dbo].[employmentTypes] OFF
